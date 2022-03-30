6bd5aeb8df6cf9c2af1b52a2c546c49fadd1714b
Sura Watthanalamlert <surawatthana@hotmail.com>
2022-03-30 16:50:08 +0700

    to trigger travis ci

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with James Gordon
+          Learn React with Me
         </a>
       </header>
     </div>
